89e2106d2e50fc599974b0187c143bd85d3f6838
tikarakas <159794811+tikarakas@users.noreply.github.com>
2024-02-12 22:51:19 +0300

    Create kodland.py

---
+++ kodland.py
@@ -0,0 +1,13 @@
+meme_dict = {
+            "CRINGE": "Garip ya da utandırıcı bir şey",
+            "LOL": "Komik bir şeye verilen cevap",
+            "ROFL":"Bir şakaya karşılık cevap",
+            "SHEESH":"Onaylamamak",
+            }
+            
+word = input("Anlamadığınız bir kelime yazın (hepsini büyük harflerle yazın!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Öyle bir kelime yok!!!")
